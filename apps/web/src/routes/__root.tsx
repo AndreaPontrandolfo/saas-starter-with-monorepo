e0a6a2966bd94cc0e3ff40e34c4cf8a92d27d317
AndreaPontrandolfo <andrea.pontra@gmail.com>
2026-01-10 20:32:43 +0100

    Add polyfill script to RootDocument for improved compatibility

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -52,6 +52,10 @@ function RootDocument() {
         <AuthProvider>
           <html lang="en">
             <head>
+              <script
+                src="https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?version=4.8.0"
+                crossOrigin="anonymous"
+              />
               <HeadContent />
             </head>
             <body>
